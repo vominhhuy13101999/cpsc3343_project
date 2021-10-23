11c19cba92e5929d112d72470034d11464280dde
root <vominhhuy13101999@gmail.com>
2021-10-23 05:34:06 +0000

    finally

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include "bubble.h"
+
 
 int main()
 {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr)/sizeof(arr[0]);
-    bubble b;
-    b.Sort(&arr,n);
-    cout<<"Sorted array: \n";
-    print(arr, n);
+    
+    cout<<"Sorted array: \n"<<arr[0]<<endl;
+    
     return 0;
 }
--- sort.cpp
+++ sort.cpp
@@ -4,14 +4,14 @@ using namespace std;
 #include "bubble.h"
 
 
-void bubble::Sort(int arr[],int n) {
+void bubble::Sort(int (&arr)[],int n) {
 
 //sort int array
     // int size=sizeof(arr)/sizeof(arr[0]);
     for (int i=0;i<n-1;i++){
         for (int j=0;j<n-i-1;j++){
             if (arr[j]<arr[j+1]){
-                swap(&arr[j],&arr[j+1]);
+                swap(arr[j],arr[j+1]);
             }
         }
     }
