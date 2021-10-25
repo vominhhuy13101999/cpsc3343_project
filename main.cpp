b90b689394bca3b9df37050288414ca2a990fd41
root <vominhhuy13101999@gmail.com>
2021-10-25 01:53:35 +0000

    add more data set

--- main.cpp
+++ main.cpp
@@ -13,14 +13,14 @@ using namespace std;
 int main()
 {
     srand(time(NULL));
-    int size=130000;
+    int size=250000;
     int sample_size=5;
     int** carrier=generate_carrier(size,sample_size);
-    string filename="./data/data_130000.txt";
+    string filename="./data/data_250000.txt";
     print(carrier,sample_size);
     cout<<filename<<endl;
     read_dataset(carrier,filename,size,sample_size);
-    int* arr=carrier[1];
+    int* arr=carrier[4];
     print(arr,size);
 
 
